79d6cf5d00fc9bf4231af95d7fa78c3e2fd71193
Артём Минидогов <mr.pon4ik337@mail.ru>
2023-04-18 22:10:41 +0300

    dasdas

--- Sonastik_TARpv21/Sonastik_TARpv21.Android/MainActivity.cs
+++ Sonastik_TARpv21/Sonastik_TARpv21.Android/MainActivity.cs
@@ -7,7 +7,7 @@ using Android.OS;
 
 namespace Sonastik_TARpv21.Droid
 {
-    [Activity(Label = "Sonastik_TARpv21", Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize )]
+    [Activity(Label = "Sonastik_TARpv21", Icon = "@drawable/ico", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize )]
     public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
     {
         protected override void OnCreate(Bundle savedInstanceState)
